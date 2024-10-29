213c897a8eb9b2860c47d41d8f4c3347c66c7209
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-10-29 20:22:03 -0300

    Add more pizzas

--- src/pages/Menu/menuCategories/index.ts
+++ src/pages/Menu/menuCategories/index.ts
@@ -3,132 +3,257 @@ import type Category from "./types/Category"
 import dishes from "./dishes"
 
 const pizzaCategory: Category = {
-  name: "Pizzas",
+  name: 'Pizzas',
   items: [
     {
-      name: "À Moda Do Chefe",
-      price: "85,00",
-      description: "Frango, Milho, Ervilha, Calabresa, Presunto, Catupiry, Parmesão E Azeitona"
+      name: 'À Moda Do Chefe',
+      price: '85,00',
+      description: 'Frango, Milho, Ervilha, Calabresa, Presunto, Catupiry, Parmesão E Azeitona'
     },
     {
-      name: "Alho",
-      price: "80,00",
-      description: "Molho De Tomate, Orégano, Alho Frito, Parmesão E Azeitona"
+      name: 'Alho',
+      price: '80,00',
+      description: 'Molho De Tomate, Orégano, Alho Frito, Parmesão E Azeitona'
     },
     {
-      name: "Aliche",
-      price: "80,00",
-      description: "Mussarela E Aliche Importado"
+      name: 'Aliche',
+      price: '80,00',
+      description: 'Mussarela E Aliche Importado'
     },
     {
-      name: "À Marinheira",
-      price: "107,00",
-      description: "Mussarela Com Frutos Do Mar"
+      name: 'À Marinheira',
+      price: '107,00',
+      description: 'Mussarela Com Frutos Do Mar'
     },
     {
-      name: "À Moda",
-      price: "77,00",
-      description: "Presunto, Palmito, Ovo, Ervilha, Mussarela, Bacon E Azeitona"
+      name: 'À Moda',
+      price: '77,00',
+      description: 'Presunto, Palmito, Ovo, Ervilha, Mussarela, Bacon E Azeitona'
     },
     {
-      name: "À Moda Do Pizzaiolo",
-      price: "85,00",
-      description: "Presunto, Palmito, Catupiry, Bacon, Orégano E Azeitona"
+      name: 'À Moda Do Pizzaiolo',
+      price: '85,00',
+      description: 'Presunto, Palmito, Catupiry, Bacon, Orégano E Azeitona'
     },
     {
-      name: "Atum",
-      price: "77,00",
-      description: "Atum E Cebola"
+      name: 'Atum',
+      price: '77,00',
+      description: 'Atum E Cebola'
     },
     {
-      name: "Atum Com Catupiry",
-      price: "80,00",
-      description: "Atum, Catupiry, Molho De Tomate, Orégano E Azeitona"
+      name: 'Atum Com Catupiry',
+      price: '80,00',
+      description: 'Atum, Catupiry, Molho De Tomate, Orégano E Azeitona'
     },
     {
-      name: "Atum Especial",
-      price: "80,00",
-      description: "Mussarela, Atum E Cebola"
+      name: 'Atum Especial',
+      price: '80,00',
+      description: 'Mussarela, Atum E Cebola'
     },
     {
-      name: "Bacon",
-      price: "75,00",
-      description: "Mussarela E Bacon"
+      name: 'Bacon',
+      price: '75,00',
+      description: 'Mussarela E Bacon'
     },
     {
-      name: "Baiana",
-      price: "75,00",
-      description: "Mussarela, Calabresa Apimentada Moída E Cebola"
+      name: 'Baiana',
+      price: '75,00',
+      description: 'Mussarela, Calabresa Apimentada Moída E Cebola'
     },
     {
-      name: "Bauru",
-      price: "75,00",
-      description: "Mussarela, Presunto, Tomate Em Rodelas E Orégano"
+      name: 'Bauru',
+      price: '75,00',
+      description: 'Mussarela, Presunto, Tomate Em Rodelas E Orégano'
     },
     {
-      name: "Beira Mar",
-      price: "75,00",
-      description: "Mussarela, Tomate, Cebola, Parmesão E Azeitona"
+      name: 'Beira Mar',
+      price: '75,00',
+      description: 'Mussarela, Tomate, Cebola, Parmesão E Azeitona'
     },
     {
-      name: "Boa Noite",
-      price: "80,00",
-      description: "Catupiry Com Milho Verde, Ervilha E Azeitona"
+      name: 'Boa Noite',
+      price: '80,00',
+      description: 'Catupiry Com Milho Verde, Ervilha E Azeitona'
     },
     {
-      name: "Brasileira",
-      price: "80,00",
-      description: "Palmito E Catupiry"
+      name: 'Brasileira',
+      price: '80,00',
+      description: 'Palmito E Catupiry'
     },
     {
-      name: "Brócolis",
-      price: "80,00",
-      description: "Brócolis Com Bacon Coberto Com Mussarela"
+      name: 'Brócolis',
+      price: '80,00',
+      description: 'Brócolis Com Bacon Coberto Com Mussarela'
     },
     {
-      name: "Picanha Defumada",
-      price: "95,00",
-      description: "Mussarela De Búfala E Catupiry"
+      name: 'Picanha Defumada',
+      price: '95,00',
+      description: 'Mussarela De Búfala E Catupiry'
     },
     {
-      name: "Filé Mignon",
-      price: "95,00",
-      description: "Mussarela Coberta Com Gorgonzola"
+      name: 'Filé Mignon',
+      price: '95,00',
+      description: 'Mussarela Coberta Com Gorgonzola'
     },
     {
-      name: "Caipira",
-      price: "80,00",
-      description: "Frango, Milho, Cebola E Catupiry"
+      name: 'Caipira',
+      price: '80,00',
+      description: 'Frango, Milho, Cebola E Catupiry'
     },
     {
-      name: "Calabresa",
-      price: "48,00",
-      description: "Calabresa, Cebola E Azeitona"
+      name: 'Calabresa',
+      price: '48,00',
+      description: 'Calabresa, Cebola E Azeitona'
     },
     {
-      name: "Calzone Fechado",
-      price: "75,00",
-      description: "Mussarela, Ricota, Presunto, Molho De Tomate, Parmesão Gratinado"
+      name: 'Calzone Fechado',
+      price: '75,00',
+      description: 'Mussarela, Ricota, Presunto, Molho De Tomate, Parmesão Gratinado'
     },
     {
-      name: "Camarão 1",
-      price: "107,00",
-      description: "Camarão E Mussarela"
+      name: 'Camarão 1',
+      price: '107,00',
+      description: 'Camarão E Mussarela'
     },
     {
-      name: "Camarão 2",
-      price: "109,00",
-      description: "Camarão E Catupiry"
+      name: 'Camarão 2',
+      price: '109,00',
+      description: 'Camarão E Catupiry'
     },
     {
-      name: "Camarão Rosa 1",
-      price: "211,00",
-      description: "Camarão Rosa Médio E Mussarela"
+      name: 'Camarão Rosa 1',
+      price: '211,00',
+      description: 'Camarão Rosa Médio E Mussarela'
     },
     {
-      name: "Camarão Rosa 2",
-      price: "221,00",
-      description: "Camarão Rosa Médio E Catupiry"
+      name: 'Camarão Rosa 2',
+      price: '221,00',
+      description: 'Camarão Rosa Médio E Catupiry'
+    },
+    {
+      name: 'Carne Seca 1',
+      description: 'carne seca e mussarela',
+      price: '95,00'
+    },
+    {
+      name: 'Carne Seca 2',
+      description: 'carne seca e catupiry',
+      price: '109,00'
+    },
+    {
+      name: 'Canadense',
+      description: 'mussarela, lombo e cebola',
+      price: '75,00'
+    },
+    {
+      name: 'Carne de Siri',
+      description: 'mussarela, siri e parmesão',
+      price: '96,00'
+    },
+    {
+      name: 'Calatu',
+      description: 'catupiry, calabresa moída, rodelas de tomate, parmesão, orégano e azeitona',
+      price: '85,00'
+    },
+    {
+      name: 'Carioca',
+      description: 'frango, palmito, milho, mussarela, orégano e azeitona',
+      price: '85,00'
+    },
+    {
+      name: 'Catupiry',
+      description: 'molho de tomate, orégano e azeitona',
+      price: '80,00'
+    },
+    {
+      name: 'Cinco Queijos',
+      description: 'mussarela, catupiry, provolone, parmesão e gorgonzola',
+      price: '85,00'
+    },
+    {
+      name: 'Cogumelo',
+      description: 'mussarela, creme rosé e champignon',
+      price: '80,00'
+    },
+    {
+      name: 'Dois Queijos',
+      description: 'mussarela, catupiry, orégano e azeitona',
+      price: '75,00'
+    },
+    {
+      name: 'Escarola',
+      description: 'mussarela, tomate, escarola, alho, bacon, cebola, mussarela e orégano',
+      price: '80,00'
+    },
+    {
+      name: 'Executiva',
+      description: 'frango desfiado, bacon, provolone, orégano e azeitona',
+      price: '80,00'
+    },
+    {
+      name: 'Frango com Catupiry',
+      description: 'frango, catupiry, orégano e azeitona',
+      price: '80,00'
+    },
+    {
+      name: 'Gorgonzola',
+      description: 'gorgonzola com molho de tomate',
+      price: '80,00'
+    },
+    {
+      name: 'Lombo com Catupiry',
+      description: 'lombo, catupiry, molho de tomate, orégano e azeitona',
+      price: '80,00'
+    },
+    {
+      name: 'Macaca',
+      description: 'mussarela, banana, catupiry e canela',
+      price: '80,00'
+    },
+    {
+      name: 'Mafiosa',
+      description: 'mussarela, rodelas de tomate, parmesão e azeitona',
+      price: '75,00'
+    },
+    {
+      name: 'Mama Celeste',
+      description: 'escarola, ricota, mussarela, alho, molho de tomate e orégano',
+      price: '80,00'
+    },
+    {
+      name: 'Mama Mia',
+      description: 'presunto, ovos, ervilha, palmito, cebola, catupiry, orégano e azeitona',
+      price: '75,00'
+    },
+    {
+      name: 'Marajá',
+      description: 'calabresa, lombinho canadense, cebola e orégano',
+      price: '80,00'
+    },
+    {
+      name: 'Mascote',
+      description: 'mussarela de búfala, rúcula e tomate seco',
+      price: '80,00'
+    },
+    {
+      name: 'Milho e Catupiry',
+      description: 'milho e catupiry',
+      price: '80,00'
+    },
+    {
+      name: 'Milho e Mussarela',
+      description: 'milho e mussarela',
+      price: '80,00'
+    },
+    {
+      name: 'Miti',
+      description: 'presunto, ervilha, ovo, mussarela, aliche e orégano',
+      price: '80,00'
+    },
+    {
+      name: 'Mussarela',
+      description: 'mussarela, orégano e rodelas de tomate',
+      price: '56,00'
     }
   ]
 }
